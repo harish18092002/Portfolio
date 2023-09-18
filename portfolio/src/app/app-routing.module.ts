ec441fbeae42976d1b2213d2637b171da025d144
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-18 21:57:41 +0530

    Header

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationComponent } from './certification/certification.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'skills', component: SkillsComponent},
+      { path: 'certification', component:CertificationComponent},
+      { path: 'projects', component: ProjectsComponent},
+      { path: 'contact', component:ContactComponent},
+      { path: '**', redirectTo: 'home' },]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationComponent } from './certification/certification.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    SkillsComponent,
+    CertificationComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
